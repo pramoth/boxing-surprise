11a567a917ca5a49d654a66ff572a7703e72ca02
pramoth <pramoth@geniustree.co.th>
2015-07-19 20:16:31 +0700

    init

---
+++ src/main/java/th/co/geniustree/internship/boxing/surprise/App.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.co.geniustree.internship.boxing.surprise;
+
+/**
+ *
+ * @author pramoth
+ */
+public class App {
+    public static void main(String[] args) {
+        Integer a=1;
+        Integer b=1;
+        System.out.println("x==y => "+(a==b));
+        
+        Integer x=150;
+        Integer y=150;
+        System.out.println("x==y => "+(x==y));
+    }
+}
